48cb7e44d72428afcc96b9f91fee4d32b19a465f
wilson688 <sowmyawilson68@gmail.com>
2021-03-09 01:05:26 -0500

    base code

---
+++ java-api/src/main/java/com/todolist/org/service/serviceImp/ListServiceImp.java
@@ -0,0 +1,22 @@
+package com.todolist.org;
+
+
+
+public class ListServiceImp {
+
+
+    public void add(String value) {
+        ToDoList tdl = new ToDoList(value);
+    }
+
+
+    public void delete(int id) {
+        //delete
+    }
+
+    public Map<Integer, String> showList() {
+
+    }
+
+    public
+}
